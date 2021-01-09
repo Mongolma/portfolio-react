c4e06fbe3c4ea367a70cbe7ad0e16263593e4296
mongolmaa <mongolmaa@mongolmaas-MBP.home>
2021-01-09 18:17:27 -0500

    text-align issue

--- portfolio/src/components/HeroSection/index.js
+++ portfolio/src/components/HeroSection/index.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import Video from '../../Videos/video.mp4'
-import {HeroContainer, Herobg, Videobg} from './HeroElements';
+import React, {useState} from 'react';
+import Video from '../../Videos/video.mp4';
+import {Button} from "../ButtonElement";
+import {HeroContainer, Herobg, Videobg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements';
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer id='home'>
             <Herobg>
                 <Videobg autoPlay loop muted src={Video} type='video.mp4'/>
             </Herobg>
+            <HeroContent>
+                <HeroH1>Mongolmaa Gurbazar</HeroH1>
+                    <HeroP>I'm full stack web developer</HeroP>
+                         <HeroBtnWrapper>
+                                <Button to='project' onMouseEnter={onHover} onMouseLeave={onHover}
+                                primary='true' dark='true'>
+                                Project {hover ? <ArrowForward /> : <ArrowRight />}
+                            </Button>    
+                    </HeroBtnWrapper>
+         </HeroContent>
         </HeroContainer>
+        
     )
 }
 
