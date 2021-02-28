d042d28bd4eb54f3ca438051b222b83e529565b4
mongolmaa <mongolmaa@mongolmaas-MBP.home>
2021-02-27 23:13:24 -0500

    new builded version4

--- portfolio/src/components/SideBar/Sidebar.js
+++ portfolio/src/components/SideBar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink} from './SidebarElements';
+import {SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink} from './SidebarElements.js';
 
 
 const Sidebar = ({ isOpen, toggle }) => {
